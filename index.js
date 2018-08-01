e64d66818b7ba97e213904a282ef030e31bbedb4
Lea Ellingwood <leaellingwood@hotmail.com>
2018-08-01 12:16:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// add
+function add(x,y) {
+    console.log(x + y);
+}
+add(2,4)
+
+
+// // multiply
+
+function add(a,b){
+    return a+b  
+  }
+  
+  function multiply1(a,b){
+     let result=0
+     for (i=0;i<a;i++){
+        result = result+b
+     }  
+     return result 
+    }
+  
+  let multiply_result = multiply1(6,8)
+  console.log(multiply_result)
+
+
+// // // power
+
+
+// // // factorial
+
+
+// // fibonacci
