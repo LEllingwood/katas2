629474db24c9293e660abb0df0d1cb61c3238a58
Lea Ellingwood <leaellingwood@hotmail.com>
2018-08-01 16:34:50 -0400

    second

--- index.js
+++ index.js
@@ -1,32 +1,94 @@
 // add
 function add(x,y) {
-    console.log(x + y);
+    return x + y;
 }
-add(2,4)
+const result = add(2,4)
+console.log(result)
 
 
 // // multiply
 
-function add(a,b){
-    return a+b  
-  }
-  
-  function multiply1(a,b){
-     let result=0
-     for (i=0;i<a;i++){
-        result = result+b
+  function multiply(a,b){
+     let result=0;
+     for (let i = 0;i < a;i++){
+        result = result+b;
+
      }  
+     
      return result 
     }
+    
+  console.log(multiply(6,8))
   
-  let multiply_result = multiply1(6,8)
-  console.log(multiply_result)
 
 
 // // // power
 
+function power(c,d) {
+    
+    let result=1;
+    // debugger;
+    for (let i = 0; i < d; i++){
+       result = multiply(result,c)
+    }  
+
+    return result; 
+   }
+ 
+console.log(power(2,8))
+
 
 // // // factorial
 
+function factorial(e) {
+    
+    let result=1;
+    debugger;
+    for (let i = 1; i < e; i++)
+     {
+       result = multiply(result,i)
+    }  
+
+    return result; 
+   }
+ 
+console.log(factorial(5));
 
 // // fibonacci
+
+// funtion fibonacci(n) {
+
+    // function testNum(a) {
+    //     if (n === 0) {
+    //       return 0;
+        
+    //     else 
+    //       if (n === 1)
+    //       return 1;
+        
+        
+    //     }
+    //   }
+      
+// if n = 1 || n = 0
+//    return n;
+
+// else
+//     return F(n-1) + F(n-2);
+
+//       console.log(fibonacci(8);
+
+// }
+
+function fibonacci(n)  {
+    if(n == 0)
+        return 0;
+    
+    else 
+    if(n == 1)
+      return 1;
+   
+    else
+      return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(fibonacci(7))
