476ddbd11c8ad12b61d7b79541ade587e5fe002f
Lea Ellingwood <leaellingwood@hotmail.com>
2018-08-07 11:53:48 -0400

    second

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// add
+// add  Write a function named "add" that takes two arguments and returns their sum.
+// You may use built-in operators to finish the definition.
+// Display the result of calling add(2, 4), which should be 6.
+
 function add(x,y) {
     return x + y;
 }
@@ -6,13 +9,15 @@ const result = add(2,4)
 console.log(result)
 
 
-// // multiply
+// // // multiply
+// Write a function named "multiply" that takes two arguments and returns their product.
+// You may not use built-in arithmetic operators or functions. Instead, you'll need a for loop which calls the "add" function you wrote earlier.  Display the result of calling multiply(6, 8), which should be 48.
 
   function multiply(a,b){
      let result=0;
      for (let i = 0;i < a;i++){
-        result = result+b;
-
+        result = add(result,b);
+        // result = add(6,result);
      }  
      
      return result 
@@ -22,7 +27,10 @@ console.log(result)
   
 
 
-// // // power
+// // // // power
+// Write a function named "power" that takes two arguments (x and n) and returns the the result of raising x to the nth power.
+// You may not use built-in arithmetic operators or functions. Instead, use functions you wrote in earlier katas to write this function.
+// Display the result of calling power(2, 8), which should be 256.
 
 function power(c,d) {
     
@@ -38,7 +46,10 @@ function power(c,d) {
 console.log(power(2,8))
 
 
-// // // factorial
+// // // // factorial
+// Write a function named "factorial" that takes a single argument and returns the factorial of that argument.
+// You may not use built-in arithmetic operators or functions. Instead, use functions you wrote in earlier katas to write this function.
+
 
 function factorial(e) {
     
@@ -54,41 +65,24 @@ function factorial(e) {
  
 console.log(factorial(5));
 
-// // fibonacci
+// // // fibonacci
+// Write a function named "fibonacci" that takes an argument n and returns the nth, Fibonacci number (Links to an external site.)Links to an external site. (click me!).
+// You may not use built-in arithmetic operators or functions. Instead, use functions you wrote in earlier katas to write this function.
 
-// funtion fibonacci(n) {
-
-    // function testNum(a) {
-    //     if (n === 0) {
-    //       return 0;
-        
-    //     else 
-    //       if (n === 1)
-    //       return 1;
-        
-        
-    //     }
-    //   }
-      
-// if n = 1 || n = 0
-//    return n;
-
-// else
-//     return F(n-1) + F(n-2);
-
-//       console.log(fibonacci(8);
-
-// }
-
-function fibonacci(n)  {
-    if(n == 0)
-        return 0;
+function fibonacci (n) {
     
-    else 
-    if(n == 1)
-      return 1;
-   
-    else
-      return fibonacci(n - 1) + fibonacci(n - 2);
+    let a = 0;
+    let b = 1;
+    let result = 0;
+    
+    for (i = 1; i < n-1; i++) {
+        result = add(a, b)
+        a = b;
+        b = result;
+    
+    } 
+    return result
+
 }
-console.log(fibonacci(7))
+console.log(fibonacci(8)); 
+// 
